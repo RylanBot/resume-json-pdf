27d92fdc9e08067e35fd7168fde024c044ff37f0
zuiyu1998 <1542844298@qq.com>
2024-07-08 09:25:59 +0800

    feat: 简历更改

--- src/data/zh/experience.ts
+++ src/data/zh/experience.ts
@@ -1,99 +1,89 @@
-import { ExperienceData } from "@/types/experience";
+import { ExperienceData } from '@/types/experience';
 
 const ZH_EXPERIENCE: ExperienceData[] = [
-    {
-        section: "教育经历",
-        icon: "PiGraduationCap",
-        items: [
-            {
-                title: "xx 大学",
-                subtitle: "软件工程",
-                timeline: "2021.09 - 2025.07",
-                tech: "",
-                details: [
-                    "**相关课程：**算法与数据结构（xx），计算机网络（xx），软件测试（xx），数据库（xx）等",
-                    "**荣誉奖项：**第 x 届 xxxx 大赛金奖，xx 大学 xxxx 奖学金",
-                    "**英语水平：**CET-4（xxx），CET-6（xxx）"
-                ]
-            }
-        ]
-    },
-    {
-        section: "实习经历",
-        icon: "FiUser",
-        items: [
-            {
-                title: "xxxx 技术有限公司",
-                subtitle: "xxx 部门",
-                timeline: "2023.07 - 2023.09",
-                tech: "Vue2 + Axios + Ant Design",
-                details: [
-                    "封装 **xxxxx 组件**，解决 xxxxx 问题",
-                    "使用 xxxxx，**优化 xxxxx**",
-                    "......",
-                    "......"
-                ]
-            }
-        ]
-    },
-    {
-        section: "项目经历",
-        icon: "IoCodeSlashSharp",
-        items: [
-            {
-                title: "在线简历生成器",
-                subtitle: "https://github.com/RylanBot/resume-json-pdf",
-                timeline: "2023.12 - 2024.01",
-                tech: "React + TypeScript + Tailwind CSS",
-                details: [
-                    "......",
-                    "......",
-                    "......"
-                ]
-            },
-            {
-                title: "xxxxxx",
-                subtitle: "",
-                timeline: "2023.09 - 2023.12",
-                tech: "xxx + xxxx + xxxxx",
-                details: [
-                    "......",
-                    "......",
-                    "......",
-                    "......"
-                ]
-            },
-            {
-                title: "xxxxxx",
-                subtitle: "",
-                timeline: "2023.10 - 2023.12",
-                tech: "xxx + xxxx + xxxxx",
-                details: [
-                    "......",
-                    "......",
-                    "......"
-                ]
-            }
-        ]
-    },
-    {
-        section: "专业技能",
-        icon: "PiGear",
-        items: [
-            {
-                title: "",
-                subtitle: "",
-                timeline: "",
-                tech: "",
-                details: [
-                    "......",
-                    "......",
-                    "......",
-                    "......"
-                ]
-            }
-        ]
-    }
+  {
+    section: '专业技能',
+    icon: 'PiGear',
+    items: [
+      {
+        title: '前端',
+        subtitle: 'Vue/React/RN/Flutter',
+        timeline: '2020-至今',
+        tech: 'Ant Design + TypeScript + React + Vue3 + Vben',
+        details: [
+          '扎实的HTML/HTML5、CSS/CSS3、JavaScript基础，熟悉前端性能优化和和浏览器调试',
+          '熟悉常用的前端框架和库（如：Vue、React）以及相关开发工具',
+          '了解前端工程化工具以及流程（如： webpack、Vite），能独立完成前端项目的打包部署与优化',
+          '熟悉常用的前端跨平台框架和库（如：React Native、Flutter）以及相关开发工具',
+        ],
+      },
+      {
+        title: '后端',
+        subtitle: 'Rust/Node',
+        timeline: '2020-至今',
+        tech: 'Tokio + Nom + Bevy',
+        details: [
+          '熟练Rust编程语言和Node开发',
+          '了解常用的软件架构模式及软件开发流程',
+          '熟悉Linux操作和Shell，能熟练使用基本的编程编译工具',
+          '熟悉常用数据库的使用，如Mysql, Postgresql, Sqlite',
+          '深度参与可视化和聊天类软件的实际开发',
+          '了解音视频相关协议，如rtsp/hls/flv等协议',
+        ],
+      },
+      {
+        title: '通用',
+        subtitle: '协作',
+        timeline: '2020-至今',
+        tech: 'Git + 编码习惯',
+        details: [
+          '良好的编码习惯和协作能力，熟练git,不定时参与开源',
+          '对技术有激情，喜欢钻研，能快速接受和掌握新技术，有较强的独立、主动的学习能力',
+          '扎实的文档输出能力，时常在博客上发布文章',
+        ],
+      },
+    ],
+  },
+
+  {
+    section: '教育经历',
+    icon: 'PiGraduationCap',
+    items: [
+      {
+        title: '湖南工学院',
+        subtitle: '通信工程',
+        timeline: '2016.09 - 2020.07',
+        tech: '',
+        details: [],
+      },
+    ],
+  },
+  {
+    section: '项目经历',
+    icon: 'IoCodeSlashSharp',
+    items: [
+      {
+        title: 'xiu-copy',
+        subtitle: 'https://github.com/zuiyu1998/xiu.git',
+        timeline: '2023.06 - 至今',
+        tech: 'Rust + Rtsp',
+        details: [
+          '基于xiu本身的功能添加了向主流直播网站推送流的功能，如B站',
+          '实现了对直播流的',
+        ],
+      },
+      {
+        title: 'im-demo',
+        subtitle: 'https://github.com/zuiyu1998/next-im-v1.git',
+        timeline: '2022.12 - 2023.06',
+        tech: 'Rust + Tokio + Im + Sqlite3',
+        details: [
+          '实现了一对一消息和组消息，包括实时消息传送，消息存储，用户重连',
+          '实现了好友系统，包括添加好友，处理好友请求',
+        ],
+      },
+    ],
+  },
 ];
 
 export default ZH_EXPERIENCE;
--- src/data/zh/profile.ts
+++ src/data/zh/profile.ts
@@ -1,45 +1,45 @@
-import { ProfileData } from "@/types/profile";
+import { ProfileData } from '@/types/profile';
 
 const ZH_PROFILE: ProfileData = {
-    name: "Xxx",
-    avatar: "https://s2.loli.net/2024/01/09/fJvOraZGAHyI6wW.png",
-    footnote: [
-        {
-            label: "求职意向",
-            content: "前端开发实习生"
-        },
-        {
-            label: "到岗时间",
-            content: "xx 年 x 月"
-        }
-    ],
-    contact: [
-        {
-            icon: "PiPhoneCallFill",
-            key: "电话",
-            value: "139-xxx-xxxxx"
-        },
-        {
-            icon: "HiOutlineMail",
-            key: "邮箱",
-            value: "rylanbot@foxmail.com"
-        },
-        {
-            icon: "PiWechatLogoBold",
-            key: "微信",
-            value: "xxxxxx"
-        },
-        {
-            icon: "MdRssFeed",
-            key: "博客",
-            value: "https://rylan.cn"
-        },
-        {
-            icon: "BiLogoGithub",
-            key: "",
-            value: "https://github.com/RylanBot"
-        }
-    ]
+  name: '罗武',
+  avatar: 'https://s2.loli.net/2024/01/09/fJvOraZGAHyI6wW.png',
+  footnote: [
+    {
+      label: '求职意向',
+      content: '前端、rust开发',
+    },
+    // {
+    //   label: '到岗时间',
+    //   content: '2024 年 x 月',
+    // },
+  ],
+  contact: [
+    {
+      icon: 'PiPhoneCallFill',
+      key: '电话',
+      value: '15576706916',
+    },
+    {
+      icon: 'HiOutlineMail',
+      key: '邮箱',
+      value: '1542844298@qq.com',
+    },
+    {
+      icon: 'PiWechatLogoBold',
+      key: '微信',
+      value: 'bj15576706916',
+    },
+    {
+      icon: 'MdRssFeed',
+      key: '博客',
+      value: 'https://zuiyu1998.github.io/',
+    },
+    {
+      icon: 'BiLogoGithub',
+      key: '',
+      value: 'https://github.com/zuiyu1998',
+    },
+  ],
 };
 
 export default ZH_PROFILE;
