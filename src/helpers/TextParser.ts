5b837709e16a2ad2960a7309bcd6956252eb4873
Rylan <1217013295@qq.com>
2024-07-28 19:30:36 +0800

    🐛 fix: adjust the link rule to exclude special characters

--- src/helpers/TextParser.ts
+++ src/helpers/TextParser.ts
@@ -7,7 +7,7 @@ type Rule = [RegExp, string];
 const rules: Rule[] = [
   [/\*\*([^\n]+?)\*\*/g, "<b class='theme-text-color opacity-90'>\$1</b>"], // 加粗
   [/\*([^\n]+?)\*/g, "<i>\$1</i>"], // 斜体
-  [/(https?:\/\/)([^\s]+)/g, "<a href='\$1\$2' target='_blank'>\$2</a>"], // 链接
+  [/(https?:\/\/)([\w\d./:-]+)/g, "<a href='\$1\$2' target='_blank'>\$2</a>"], // 链接
   [/\`([^\n]+?)\`/g, "<span class='bg-gray-100 rounded py-0.5 px-2 text-xs font-mono'>$1</span>"] // 行内代码
 ];
 
