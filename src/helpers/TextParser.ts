8898e98f4a66344188ef3320c7df370222ec473d
Rylan <rylanbot@qq.com>
2025-07-12 04:35:57 +0800

    ✨ feat: support underscore for bold and italic markdown parsing

--- src/helpers/TextParser.ts
+++ src/helpers/TextParser.ts
@@ -5,8 +5,8 @@ import useModeStore from "@/stores/modeStore";
 type Rule = [RegExp, string];
 
 const rules: Rule[] = [
-  [/\*\*([^\n]+?)\*\*/g, "<b class='theme-text-color opacity-90'>$1</b>"], // 加粗
-  [/\*([^\n]+?)\*/g, "<i>$1</i>"], // 斜体
+  [/(\*\*|__)(.*?)\1/g, "<b class='theme-text-color opacity-90'>$2</b>"], // 加粗
+  [/(\*|_)(.*?)\1/g, "<i>$2</i>"], // 斜体
   [/(https?:\/\/)([\w\d./:-]+)/g, "<a href='$1$2' target='_blank'>$2</a>"], // 链接
   [/`([^\n]+?)`/g, "<span class='bg-gray-100 rounded py-0.5 px-2 text-xs font-mono'>$1</span>"] // 行内代码
 ];
